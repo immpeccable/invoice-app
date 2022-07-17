1b1a450a109b8247e1778fd3454572e55a970fe0
tunahan dündar <84476929+immpeccable@users.noreply.github.com>
2022-07-17 18:58:26 +0300

    hierarchy created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import MainPage from "./components/MainPage";
+import "./Router.css"
 
 
 function MainRouter() {
   return (
-    <div>
-      hello
+    <div className="root-wrapper">
+      <Header></Header>
+      <Routes>
+        <Route exact path = "/" element = {<MainPage></MainPage>}></Route>
+      </Routes>
     </div>
   );
 }
